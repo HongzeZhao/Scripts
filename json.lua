d799d8d4a7ffe910816cb929ca633b7590a12b39
Hongze ZHAO <hongze.zhao@gmail.com>
2015-01-13 00:27:45 +0800

    finished marshal part, not yet pass

--- json.lua
+++ json.lua
@@ -1,10 +1,10 @@
---[[
+--[===============================================================[
 	Author: Hongze Zhao (honze@163.com)
 	Date: 2015-1-10
 	Module Description:
-		This is a converter module of exchanging json data string and
-		lua table value.
-]]
+		This is a converter module of exchanging json data string
+		and lua table value.
+  ]===============================================================]
 
 module("json", package.seeall)
 
@@ -16,36 +16,103 @@ json_table_end = string.byte('}')
 json_array_begin = string.byte('[')
 json_array_end = string.byte(']')
 json_quote = string.byte('"')         -- only double quote is allowed in json
+json_split = string.byte(',')
 
 
 -- get the key string of a json object (table) item, i is the start index
--- where the key begins. A valid name string should be quoted.
+-- where the key begins. A valid name string should be quoted with ".
 local parse_keyname = function ( json_str, i )
-	-- body
+	return string.find(json_str, "\"(.-)\"%s*:", i)
 end
 
-local marshal_internal = function( json_str, i)
-	local retval = {} -- a table or array
+-- parse the value string and get its value
+-- last matched index returned
+local parse_valuestr = function ( json_str, i )
+ 	return string.find(json_str, "[,%[%{]?%s*(.-)%s*[,%]%}]", i)
+end
 
-	local begin_char = string.byte(json_str, i)
-	if begin_char == json_table_begin then
-
-		retval["a"] = 1
-
-	elseif begin_char == json_array_begin then
-
-		retval[1] = 1
-		
+-- conver value string to coresponding lua value
+local to_value = function ( valstr )
+	if string.byte(valstr) == json_quote then  -- a string value
+		return string.sub(valstr, 2, -2)
+	elseif valstr == "null" then -- nil value
+		return nil
+	elseif valstr == "true" then
+		return true
+	elseif valstr == "false" then
+		return false
 	else
-		print("Error: Unknown begin char.")
+		return tonumber(valstr)
 	end
+end
 
-	return retval
+-- get first unempty char
+local get_firstchar = function ( json_str, i )
+	return string.match(json_str, "^%s*(%S)", i)
+end
+
+local parse_array_item = function ( tstack, json_str, i )
+	
+end
+
+local parse_table_item = function ( tstack, json_str, i )
+	-- get the top table
+	local t = tstack[#tstack]
+
+	
+
 end
 
 -- convert json data string to lua table
 function Marshal( json_str )
-	
+	local tstack = {{}}
+	local strlen = #json_str
+	local mode = true  -- true:table ; false:array
+
+	for i = 1, strlen do
+		-- get the top table
+		local t = tstack[#tstack]
+
+		-- find the first bracket symbol
+		local j = string.find(json_str, "^%s*[,%{%[%]%}]", i)
+		local initial_char = string.byte(json_str, j)
+
+		-- a table key-value table
+		if initial_char == json_table_begin or    -- {
+			initial_char == json_array_begin or   -- [
+			initial_char == json_split then       -- ,
+			
+			local k, l, keyname, valuestr
+			if initial_char == json_table_begin then
+				mode = true
+				-- get key name
+				k, l, keyname = parse_keyname(json_str, j + 1)
+				i = l + 1 -- update current index
+			else
+				mode = false
+				keyname = tostring(#t + 1)
+				i = j + 1
+			end
+
+			-- whether value or table
+			local firstchar = get_firstchar(json_str, i)
+			if firstchar == json_table_begin or firstchar == json_array_begin then
+				-- create a new table and push it to stack top
+				t[keyname] = {}
+				tstack[#tstack + 1] = t[keyname]
+			else
+				k, l, valuestr = parse_valuestr(json_str, i)
+				t[keyname] = to_value(valuestr) -- ? how about nil
+				i = l + 1 -- update index
+			end
+
+		else -- ] or }
+			-- pop top table element
+			tstack[#tstack] = nil
+		end
+	end
+
+	return tstack[1]
 end
 
 -------------------------- Unmarshal ---------------------------
--- json_test.lua
+++ json_test.lua
@@ -1,21 +1,26 @@
 -- test cases
 require "json"
 
-function test( s )
-	local t = json.Marshal(s)
-	print(t.Jack[1], t.Jack[2], t.Jack[3])
-	print(t["\""][1], t["\""][2], t["\""][3])
-end
-
 do
 	local s = [==[
 	{
-		"Jack":[180, 75.4, "Beijing"],
-		"\"":[175, 70.3, "古巴"]
+		"Jack" 	:
+		[180, 75.4, "Beijing"],
+		"\"" 	:
+		[175, 70.3, "古巴"]
 	}
 	]==]
 
-	test(s)
+	--print(json.parse_keyname(s, 1))
+	--print(json.parse_keyname(s, 40))
+
+	--print(json.parse_valuestr(s, 22))
+
+	--print(string.find(s, "[%{%[]ff", 5))
+
+	local t = json.Marshal(s)
+	print(t.Jack[1], t.Jack[2], t.Jack[3])
+	print(t["\""][1], t["\""][2], t["\""][3])
 end
 
 do
@@ -36,5 +41,4 @@ do
 	}
 	]==]
 
-	test(s)
 end
