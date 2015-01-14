c4c86d77feb7623ad3c505e44eb36ff022ae0d04
Hongze ZHAO <hongze.zhao@gmail.com>
2015-01-14 23:58:11 +0800

    finished unmarshal

--- json_test.lua
+++ json_test.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- test cases
 require "json"
 
@@ -55,52 +54,4 @@ if true then
 	assert(t.first[4] == "癔")
 
 	assert(t.enemy.special[1] == "/*[\",汉]")
-=======
--- test cases
-require "json"
-
-print(_VERSION)
-
-if false then
-	local s = [==[
-	{
-		"Jack" 	:
-		[180, 75.4, "Beijing"],
-		"\"" 	:
-		[175, 70.3, "古巴\u6c49"]
-	}
-	]==]
-
-	local t = json.Marshal(s)
-
-	print(t.Jack[1], t.Jack[2], t.Jack[3])
-	print(t["\""][1], t["\""][2], t["\""][3])
-end
-
-do
-	s = [==[
-	{
-	    "4\"": "ff",
-	    "first": [
-	        2,
-	        "hello",
-	        7.03,
-	        "\u7654"
-	    ],
-	    "enemy": {
-	        "special": [
-	            "/*[\",]"
-	        ]
-	    }
-	}
-	]==]
-
-	local t = json.Marshal(s)
-
-	assert(t["4\""] == "ff")
-	for _, x in ipairs(t.first) do
-		io.write(tostring(x) .. ", ")
-	end
-	io.write("\n")
->>>>>>> FETCH_HEAD
 end
