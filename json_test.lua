65f95200a942db4c3c46199a987fea25e71a81be
honze <hongze.zhao@gmail.com>
2015-01-14 21:58:42 +0800

    added unmarshal

--- json_test.lua
+++ json_test.lua
@@ -3,7 +3,7 @@ require "json"
 
 print(_VERSION)
 
-if false then
+if true then
 	local s = [==[
 	{
 		"Jack" 	:
@@ -15,23 +15,28 @@ if false then
 
 	local t = json.Marshal(s)
 
-	print(t.Jack[1], t.Jack[2], t.Jack[3])
-	print(t["\""][1], t["\""][2], t["\""][3])
+	assert(t.Jack[1] == 180)
+	assert(t.Jack[2] == 75.4)
+	assert(t.Jack[3] == "Beijing")
+
+	assert(t["\""][1] == 175)
+	assert(t["\""][2] == 70.3)
+	assert(t["\""][3] == "古巴汉")
 end
 
-do
+if true then
 	s = [==[
 	{
 	    "4\"": "ff",
 	    "first": [
 	        2,
 	        "hello",
-	        7.03,
+	        5.0e-5,
 	        "\u7654"
 	    ],
 	    "enemy": {
 	        "special": [
-	            "/*[\",]"
+	            "/*[\",\u6c49]"
 	        ]
 	    }
 	}
@@ -39,9 +44,14 @@ do
 
 	local t = json.Marshal(s)
 
+	print(json.Unmarshal(t))
+
 	assert(t["4\""] == "ff")
-	for _, x in ipairs(t.first) do
-		io.write(tostring(x) .. ", ")
-	end
-	io.write("\n")
+	
+	assert(t.first[1] == 2)
+	assert(t.first[2] == "hello")
+	assert(t.first[3] == 5.0e-5)
+	assert(t.first[4] == "癔")
+
+	assert(t.enemy.special[1] == "/*[\",汉]")
 end
